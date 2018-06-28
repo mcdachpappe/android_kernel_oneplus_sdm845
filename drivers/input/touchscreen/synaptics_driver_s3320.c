dac94bc1f64a106f0a08f96c072022cba2a97535
Siarhei Vishniakou <svv@google.com>
2018-06-28 10:53:18 +0100

    s3320: require low latency
    Currently, CPU can enter deep sleep while waiting for data on the i2c
    bug. That means an i2c read of 8 bytes can take as long as 10 ms, which
    would overrun the desired interrupt handling time.
    Use pm qos to require low latency and prevent CPU from entering deep
    sleep while handling touch interrupts.
    
    Bug: 110939384
    Test: look at the i2c read traces:
    1) ./external/chromium-trace/systrace.py --atrace-categories=view,wm,am,app,gfx,i2c,sched,irq,video,power,input,workq,freq
    2) perform a fling
    3) review the i2c read duration on the touchscreen bus
    
    Change-Id: Icf8ab09324003a85af70517769ced3bae52f705c
    Signed-Off-By: Siarhei Vishniakou <svv@google.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -25,6 +25,7 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/hrtimer.h>
+#include <linux/pm_qos.h>
 #include <linux/proc_fs.h>
 #include <linux/interrupt.h>
 #include <linux/regulator/consumer.h>
@@ -557,6 +558,8 @@ struct synaptics_ts_data {
 	unsigned int fp_aod_cnt;
 	unsigned int unlock_succes;
 	int project_version;
+
+	struct pm_qos_request pm_qos_req;
 };
 
 static struct device_attribute attrs_oem[] = {
@@ -1995,6 +1998,9 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 	if (unlikely(ts->enable_remote))
 		goto END;
 
+	/* prevent CPU from entering deep sleep */
+	pm_qos_update_request(&ts->pm_qos_req, 100);
+
 	synaptics_rmi4_i2c_write_byte(ts->client, 0xff, 0x00);
 	ret = synaptics_rmi4_i2c_read_word(ts->client, F01_RMI_DATA_BASE);
 
@@ -2064,7 +2070,8 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 	}
 
  END:
-	pm_qos_remove_request(&pm_qos_req_tp);
+	pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
+
 	return IRQ_HANDLED;
 }
 
@@ -6508,6 +6515,10 @@ static int synaptics_ts_probe(struct i2c_client *client,
 	}
 #endif
 	init_synaptics_proc();
+
+	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+		PM_QOS_DEFAULT_VALUE);
+
 	TPDTM_DMESG("synaptics_ts_probe 3203: normal end\n");
 
 	bootmode = get_boot_mode();
@@ -6580,6 +6591,9 @@ static int synaptics_ts_remove(struct i2c_client *client)
 	input_free_device(ts->input_dev);
 	kfree(ts);
 	tpd_power(ts, 0);
+
+	pm_qos_remove_request(&ts->pm_qos_req);
+
 	return 0;
 }
 
