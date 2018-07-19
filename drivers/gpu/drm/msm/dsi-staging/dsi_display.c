a1b94b898614fa7883d632e97d4ef62001c6735c
Danny Baumann <dannybaumann@web.de>
2018-07-19 15:42:06 +0200

    drm: msm: Symlink main DSI display sysfs to its parent (MDSS)
    Allows user space to unambiguously find it.
    
    Change-Id: I944944e601c050c8213365301696521654af52c9
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5117,24 +5117,29 @@ static DEVICE_ATTR(display_mode, 0644,
 			sysfs_display_mode_read,
 			sysfs_display_mode_write);
 
-static struct attribute *dynamic_dsi_clock_fs_attrs[] = {
+static struct attribute *dsi_sysfs_attrs[] = {
 	&dev_attr_dynamic_dsi_clock.attr,
 	&dev_attr_hbm.attr,
 	&dev_attr_display_mode.attr,
 	NULL,
 };
-static struct attribute_group dynamic_dsi_clock_fs_attrs_group = {
-	.attrs = dynamic_dsi_clock_fs_attrs,
+static struct attribute_group dsi_sysfs_attrs_group = {
+	.attrs = dsi_sysfs_attrs,
 };
 
-static int dsi_display_sysfs_init(struct dsi_display *display)
+static int dsi_display_sysfs_init(struct dsi_display *display,
+		struct device *master)
 {
 	int rc = 0;
 	struct device *dev = &display->pdev->dev;
 
-	if (display->panel->panel_mode == DSI_OP_CMD_MODE)
-		rc = sysfs_create_group(&dev->kobj,
-			&dynamic_dsi_clock_fs_attrs_group);
+	if (display->panel->panel_mode == DSI_OP_CMD_MODE) {
+		rc = sysfs_create_group(&dev->kobj, &dsi_sysfs_attrs_group);
+		if (rc == 0 && display == primary_display)
+			rc = sysfs_create_link(&master->kobj,
+					&dev->kobj, "main_display");
+	}
+
 	pr_debug("[%s] dsi_display_sysfs_init:%d,panel mode:%d\n",
 		display->name, rc, display->panel->panel_mode);
 	return rc;
@@ -5146,8 +5151,7 @@ static int dsi_display_sysfs_deinit(struct dsi_display *display)
 	struct device *dev = &display->pdev->dev;
 
 	if (display->panel->panel_mode == DSI_OP_CMD_MODE)
-		sysfs_remove_group(&dev->kobj,
-			&dynamic_dsi_clock_fs_attrs_group);
+		sysfs_remove_group(&dev->kobj, &dsi_sysfs_attrs_group);
 
 	return 0;
 
@@ -5203,7 +5207,7 @@ static int dsi_display_bind(struct device *dev,
 	atomic_set(&display->clkrate_change_pending, 0);
 	display->cached_clk_rate = 0;
 
-	rc = dsi_display_sysfs_init(display);
+	rc = dsi_display_sysfs_init(display, master);
 	if (rc) {
 		pr_err("[%s] sysfs init failed, rc=%d\n", display->name, rc);
 		goto error;
