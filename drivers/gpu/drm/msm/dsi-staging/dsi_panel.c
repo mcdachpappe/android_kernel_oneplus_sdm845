1c7cf95f5a1a8bb656e5cfd6dd2a644f979b1691
Danny Baumann <dannybaumann@web.de>
2018-07-19 09:59:55 +0200

    drm: msm: Add support for display modes.
    Change-Id: I0e1f2bfa2348e5c94d67147c9fec4b042b0cabf4
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1902,6 +1902,10 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-panel-hbm-on-command-3",
 	"qcom,mdss-dsi-panel-hbm-on-command-4",
 	"qcom,mdss-dsi-panel-hbm-on-command-5",
+	"qcom,mdss-dsi-panel-display-srgb-color-mode-on-command",
+	"qcom,mdss-dsi-panel-display-p3-mode-on-command",
+	"qcom,mdss-dsi-panel-display-wide-color-mode-on-command",
+	"qcom,mdss-dsi-panel-dci-p3-off-command", // also disables SRGB and wide color modes
 };
 
 const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
@@ -1979,6 +1983,10 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-hbm-on-command-state",
 	"qcom,mdss-dsi-hbm-on-command-state",
 	"qcom,mdss-dsi-hbm-on-command-state",
+	"qcom,mdss-dsi-panel-display-srgb-color-mode-on-command-state",
+	"qcom,mdss-dsi-panel-display-p3-mode-on-command-state",
+	"qcom,mdss-dsi-panel-display-wide-color-mode-on-command-state",
+	"qcom,mdss-dsi-panel-dci-p3-off-command-state",
 };
 
 static int dsi_panel_get_cmd_pkt_count(const char *data, u32 length, u32 *cnt)
@@ -4416,6 +4424,9 @@ int dsi_panel_enable(struct dsi_panel *panel)
     if (panel->hbm_los_mode)
 		dsi_panel_apply_hbm_mode(panel);
 
+	if (panel->display_mode != DISPLAY_MODE_DEFAULT)
+		dsi_panel_apply_display_mode(panel);
+
 	if (panel->hbm_mode)
 		dsi_panel_set_hbm_mode(panel, panel->hbm_mode);
 
@@ -5023,6 +5034,25 @@ error:
 	return rc;
 }
 
+int dsi_panel_apply_display_mode(struct dsi_panel *panel)
+{
+	enum dsi_cmd_set_type type;
+	int rc;
+
+	switch (panel->display_mode) {
+		case DISPLAY_MODE_SRGB: type = DSI_CMD_SET_MODE_SRGB; break;
+		case DISPLAY_MODE_DCI_P3: type = DSI_CMD_SET_MODE_DCI_P3; break;
+		case DISPLAY_MODE_WIDE_COLOR: type = DSI_CMD_SET_MODE_WIDE_COLOR; break;
+		default: type = DSI_CMD_SET_MODE_DEFAULT; break;
+	}
+
+	mutex_lock(&panel->panel_lock);
+	rc = dsi_panel_tx_cmd_set(panel, type);
+	mutex_unlock(&panel->panel_lock);
+
+	return rc;
+}
+
 int dsi_panel_set_native_display_p3_mode(struct dsi_panel *panel, int level)
 {
 	int rc = 0;
@@ -5152,4 +5182,3 @@ int dsi_panel_set_customer_p3_mode(struct dsi_panel *panel, int level)
 	mutex_unlock(&panel->panel_lock);
 return rc;
 }
-
--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -175,6 +175,13 @@ struct dsi_panel_exd_config {
 	int selab;
 };
 
+enum dsi_panel_display_mode {
+	DISPLAY_MODE_DEFAULT,
+	DISPLAY_MODE_SRGB,
+	DISPLAY_MODE_DCI_P3,
+	DISPLAY_MODE_WIDE_COLOR
+};
+
 struct dsi_panel {
 	const char *name;
 	enum dsi_panel_type type;
@@ -237,6 +244,9 @@ struct dsi_panel {
 	bool is_hbm_enabled;
 	int op_force_screenfp;
 	bool dim_status;
+
+	enum dsi_panel_display_mode display_mode;
+
 	bool lp11_init;
 	bool ulps_enabled;
 	bool ulps_suspend_enabled;
@@ -332,6 +342,8 @@ int dsi_panel_disable(struct dsi_panel *panel);
 int dsi_panel_unprepare(struct dsi_panel *panel);
 
 int dsi_panel_post_unprepare(struct dsi_panel *panel);
+
+int dsi_panel_apply_display_mode(struct dsi_panel *panel);
 
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl);
 
