43cd6e5fb83c6ddd7e7afe19d211fea6598437ef
Anirudh Ghayal <aghayal@codeaurora.org>
2018-08-10 22:37:04 +0530

    [PORT] power: qpnp-smb5: Add FORCE_RECHARGE power-supply property
    Disable/re-enable charging to force a recharge.
    
    Change-Id: I417d46cc07f455792906a18f4b6dc8e5ca8ca3da
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>
    [@mcdachpappe: Taken from sm8150 (OP7/Pro): https://github.com/OnePlusOSS/android_kernel_oneplus_sm8150/commit/68e779005f58c92cf12a5e088653c05da36dcfcc]
    [Code adapted for sdm845 (OP6/T)]
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1257,6 +1257,7 @@ static enum power_supply_property smb2_batt_props[] = {
 	POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT,
 	POWER_SUPPLY_PROP_CHARGE_COUNTER,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
+	POWER_SUPPLY_PROP_FORCE_RECHARGE,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 };
 
@@ -1404,6 +1405,9 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
 		rc = smblib_get_prop_from_bms(chg, psp, val);
+		break;
+	case POWER_SUPPLY_PROP_FORCE_RECHARGE:
+		val->intval = 0;
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_enable;
@@ -1555,6 +1559,15 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_DIE_HEALTH:
 		chg->die_health = val->intval;
 		power_supply_changed(chg->batt_psy);
+		break;
+	case POWER_SUPPLY_PROP_FORCE_RECHARGE:
+			/* toggle charging to force recharge */
+			vote(chg->chg_disable_votable, FORCE_RECHARGE_VOTER,
+					true, 0);
+			/* charge disable delay */
+			msleep(50);
+			vote(chg->chg_disable_votable, FORCE_RECHARGE_VOTER,
+					false, 0);
 		break;
 	default:
 		rc = -EINVAL;
