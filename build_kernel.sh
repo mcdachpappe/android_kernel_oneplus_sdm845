7cbf6d686c49d4cfed2b887a9a1f33711d22a451
Park Ju Hyung <qkrwngud825@gmail.com>
2018-10-29 07:20:51 +0900

    build_kernel: do not ratelimit userspace writes to /dev/kmsg
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -46,7 +46,7 @@ echo "Making new boot image"
 mkbootimg \
     --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
     --ramdisk $RAMFS_TMP.cpio.gz \
-    --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 buildvariant=user' \
+    --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 buildvariant=user printk.devkmsg=on' \
     --base           0x00000000 \
     --pagesize       4096 \
     --kernel_offset  0x00008000 \
