982bde20aaed53fa2011c2e88a136f6c40417748
Park Ju Hyung <qkrwngud825@gmail.com>
2018-10-30 17:32:51 +0900

    msm-poweroff: avoid entering Qualcomm crashdump mode
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -63,7 +63,7 @@ static void scm_disable_sdi(void);
  * There is no API from TZ to re-enable the registers.
  * So the SDI cannot be re-enabled when it already by-passed.
  */
-static int download_mode = 1;
+static int download_mode = 0;
 #else
 static const int download_mode;
 #endif
