1806a45f983a264ca93649996687b7cde14c68a7
Park Ju Hyung <qkrwngud825@gmail.com>
2019-01-23 05:55:56 +0900

    build_master: provide a fastboot image as well
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -5,14 +5,17 @@ if [[ "${1}" != "skip" ]] ; then
 	./build_kernel.sh stock "$@" || exit 1
 fi
 
+VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+
 if [ -e boot.img ] ; then
-	rm arter97-kernel-"$(cat version)".zip 2>/dev/null
+	rm arter97-kernel-$VERSION.zip 2>/dev/null
 	cp boot.img kernelzip/boot.img
+	cp boot.img arter97-kernel-$VERSION.img
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-"$(cat ../version)"-tmp.zip *
-	zipalign -v 4 arter97-kernel-"$(cat ../version)"-tmp.zip ../arter97-kernel-"$(cat ../version)"-"$(date +%F | sed s@-@@g)".zip
-	rm arter97-kernel-"$(cat ../version)"-tmp.zip
+	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
+	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
+	rm arter97-kernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-"$(cat version)"-"$(date +%F | sed s@-@@g)".zip
+	ls -al arter97-kernel-$VERSION.zip
 	rm kernelzip/boot.img
 fi
