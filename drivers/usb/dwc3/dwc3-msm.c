6102ed4a3afa2b77c9e8ac33867944ef5dd1d38d
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-03 18:18:34 +0900

    Revert "dwc3-msm: Fix dwc3_drd_state_string for undefined state"
    This reverts commit eb8e53950f75ccc8f1c0f24f1321eb98391f4168.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -148,7 +148,6 @@ enum dwc3_drd_state {
 };
 
 static const char *const state_names[] = {
-	[DRD_STATE_UNDEFINED] = "undefined",
 	[DRD_STATE_IDLE] = "idle",
 	[DRD_STATE_PERIPHERAL] = "peripheral",
 	[DRD_STATE_PERIPHERAL_SUSPEND] = "peripheral_suspend",
@@ -159,7 +158,7 @@ static const char *const state_names[] = {
 static const char *dwc3_drd_state_string(enum dwc3_drd_state state)
 {
 	if (state < 0 || state >= ARRAY_SIZE(state_names))
-		return "UNKNOWN";
+		return "UNDEFINED";
 
 	return state_names[state];
 }
