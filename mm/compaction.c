7b09a99b2707c72f110eaf32ff1c70492cfa3051
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-08 02:58:16 +0900

    compaction: fix compilation on !CONFIG_ZSWAP
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1867,7 +1867,9 @@ static void compact_node(int nid)
 	}
 }
 
+#ifdef CONFIG_ZSWAP
 extern void zswap_compact(void);
+#endif
 /* Compact all nodes in the system */
 static void compact_nodes(void)
 {
@@ -1879,7 +1881,9 @@ static void compact_nodes(void)
 	for_each_online_node(nid)
 		compact_node(nid);
 
+#ifdef CONFIG_ZSWAP
 	zswap_compact();
+#endif
 }
 
 static void do_compaction(struct work_struct *work)
