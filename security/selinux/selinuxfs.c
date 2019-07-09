2332cb67d92b85e187b085c0b44cc3f636520974
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-09 23:12:07 +0900

    selinux: use stack on security_sid_to_context() if possible
    For temporary usage such as logging, there's no need to allocate
    a dedicated memory.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -518,7 +518,8 @@ static const struct file_operations sel_load_ops = {
 
 static ssize_t sel_write_context(struct file *file, char *buf, size_t size)
 {
-	char *canon = NULL;
+	char *canon;
+	char canon_buf[SELINUX_LABEL_LENGTH];
 	u32 sid, len;
 	ssize_t length;
 
@@ -530,7 +531,9 @@ static ssize_t sel_write_context(struct file *file, char *buf, size_t size)
 	if (length)
 		goto out;
 
-	length = security_sid_to_context(sid, &canon, &len);
+	canon = canon_buf;
+
+	length = security_sid_to_context_stack(sid, &canon, &len);
 	if (length)
 		goto out;
 
@@ -544,7 +547,6 @@ static ssize_t sel_write_context(struct file *file, char *buf, size_t size)
 	memcpy(buf, canon, len);
 	length = len;
 out:
-	kfree(canon);
 	return length;
 }
 
@@ -779,7 +781,8 @@ static ssize_t sel_write_create(struct file *file, char *buf, size_t size)
 	u32 ssid, tsid, newsid;
 	u16 tclass;
 	ssize_t length;
-	char *newcon = NULL;
+	char *newcon;
+	char newcon_buf[SELINUX_LABEL_LENGTH];
 	u32 len;
 	int nargs;
 
@@ -850,7 +853,8 @@ static ssize_t sel_write_create(struct file *file, char *buf, size_t size)
 	if (length)
 		goto out;
 
-	length = security_sid_to_context(newsid, &newcon, &len);
+	newcon = newcon_buf;
+	length = security_sid_to_context_stack(newsid, &newcon, &len);
 	if (length)
 		goto out;
 
@@ -864,7 +868,6 @@ static ssize_t sel_write_create(struct file *file, char *buf, size_t size)
 	memcpy(buf, newcon, len);
 	length = len;
 out:
-	kfree(newcon);
 	kfree(namebuf);
 	kfree(tcon);
 	kfree(scon);
@@ -877,7 +880,8 @@ static ssize_t sel_write_relabel(struct file *file, char *buf, size_t size)
 	u32 ssid, tsid, newsid;
 	u16 tclass;
 	ssize_t length;
-	char *newcon = NULL;
+	char *newcon;
+	char newcon_buf[SELINUX_LABEL_LENGTH];
 	u32 len;
 
 	length = task_has_security(current, SECURITY__COMPUTE_RELABEL);
@@ -910,7 +914,8 @@ static ssize_t sel_write_relabel(struct file *file, char *buf, size_t size)
 	if (length)
 		goto out;
 
-	length = security_sid_to_context(newsid, &newcon, &len);
+	newcon = newcon_buf;
+	length = security_sid_to_context_stack(newsid, &newcon, &len);
 	if (length)
 		goto out;
 
@@ -921,7 +926,6 @@ static ssize_t sel_write_relabel(struct file *file, char *buf, size_t size)
 	memcpy(buf, newcon, len);
 	length = len;
 out:
-	kfree(newcon);
 	kfree(tcon);
 	kfree(scon);
 	return length;
@@ -933,6 +937,7 @@ static ssize_t sel_write_user(struct file *file, char *buf, size_t size)
 	u32 sid, *sids = NULL;
 	ssize_t length;
 	char *newcon;
+	char newcon_buf[SELINUX_LABEL_LENGTH];
 	int i, rc;
 	u32 len, nsids;
 
@@ -964,19 +969,18 @@ static ssize_t sel_write_user(struct file *file, char *buf, size_t size)
 
 	length = sprintf(buf, "%u", nsids) + 1;
 	ptr = buf + length;
+	newcon = newcon_buf;
 	for (i = 0; i < nsids; i++) {
-		rc = security_sid_to_context(sids[i], &newcon, &len);
+		rc = security_sid_to_context_stack(sids[i], &newcon, &len);
 		if (rc) {
 			length = rc;
 			goto out;
 		}
 		if ((length + len) >= SIMPLE_TRANSACTION_LIMIT) {
-			kfree(newcon);
 			length = -ERANGE;
 			goto out;
 		}
 		memcpy(ptr, newcon, len);
-		kfree(newcon);
 		ptr += len;
 		length += len;
 	}
@@ -993,7 +997,8 @@ static ssize_t sel_write_member(struct file *file, char *buf, size_t size)
 	u32 ssid, tsid, newsid;
 	u16 tclass;
 	ssize_t length;
-	char *newcon = NULL;
+	char *newcon;
+	char newcon_buf[SELINUX_LABEL_LENGTH];
 	u32 len;
 
 	length = task_has_security(current, SECURITY__COMPUTE_MEMBER);
@@ -1026,7 +1031,9 @@ static ssize_t sel_write_member(struct file *file, char *buf, size_t size)
 	if (length)
 		goto out;
 
-	length = security_sid_to_context(newsid, &newcon, &len);
+	newcon = newcon_buf;
+
+	length = security_sid_to_context_stack(newsid, &newcon, &len);
 	if (length)
 		goto out;
 
@@ -1040,7 +1047,6 @@ static ssize_t sel_write_member(struct file *file, char *buf, size_t size)
 	memcpy(buf, newcon, len);
 	length = len;
 out:
-	kfree(newcon);
 	kfree(tcon);
 	kfree(scon);
 	return length;
@@ -1474,16 +1480,17 @@ static ssize_t sel_read_initcon(struct file *file, char __user *buf,
 				size_t count, loff_t *ppos)
 {
 	char *con;
+	char con_buf[SELINUX_LABEL_LENGTH];
 	u32 sid, len;
 	ssize_t ret;
 
 	sid = file_inode(file)->i_ino&SEL_INO_MASK;
-	ret = security_sid_to_context(sid, &con, &len);
+	con = con_buf;
+	ret = security_sid_to_context_stack(sid, &con, &len);
 	if (ret)
 		return ret;
 
 	ret = simple_read_from_buffer(buf, count, ppos, con, len);
-	kfree(con);
 	return ret;
 }
 
