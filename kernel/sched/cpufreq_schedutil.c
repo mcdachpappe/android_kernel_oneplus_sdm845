c240b50fe5620f751d6102b5eb0640bac3e55045
0ctobot <return.of.octobot@gmail.com>
2019-07-12 07:20:37 -0400

    cpufreq: schedutil: Improve init protection checks
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -512,7 +512,7 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if (!strcmp(current->comm, "init"))
+	if (!memcmp(current->comm, "init", sizeof("init")))
 		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
@@ -535,7 +535,7 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if (!strcmp(current->comm, "init"))
+	if (!memcmp(current->comm, "init", sizeof("init")))
 		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
@@ -582,7 +582,7 @@ static ssize_t iowait_boost_enable_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	bool enable;
 
-	if (!strcmp(current->comm, "init"))
+	if (!memcmp(current->comm, "init", sizeof("init")))
 		return count;
 
 	if (kstrtobool(buf, &enable))
