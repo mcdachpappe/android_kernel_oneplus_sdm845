471f18589864dbff0dd73d09cd8a96e30be3da24
0ctobot <return.of.octobot@gmail.com>
2019-11-05 04:49:26 -0500

    SchedTune/Cpuset Assist: Improve node tampering protection
    Subsequent to dd73a14 ("exec: Add node tampering blacklist function")
    
    Utilizing the task_is_booster function should prove to be much more
    comprehensive and reliable in protecting our in-kernel
    configurations from external interference rather than simply
    checking comm for init.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: mcdachpappe <noreference@web.de>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1,3 +1,4 @@
+#include <linux/binfmts.h>
 #include <linux/cgroup.h>
 #include <linux/err.h>
 #include <linux/percpu.h>
@@ -991,7 +992,7 @@ boost_slots_release(struct schedtune *st)
 static int sched_boost_override_write_wrapper(struct cgroup_subsys_state *css,
 					      struct cftype *cft, u64 override)
 {
-	if (!strcmp(current->comm, "init"))
+	if (task_is_booster(current))
 		return 0;
 
 	return sched_boost_override_write(css, cft, override);
@@ -1000,7 +1001,7 @@ static int sched_boost_override_write_wrapper(struct cgroup_subsys_state *css,
 static int sched_colocate_write_wrapper(struct cgroup_subsys_state *css,
 					struct cftype *cft, u64 colocate)
 {
-	if (!strcmp(current->comm, "init"))
+	if (task_is_booster(current))
 		return 0;
 
 	return sched_colocate_write(css, cft, colocate);
@@ -1010,7 +1011,7 @@ static int sched_colocate_write_wrapper(struct cgroup_subsys_state *css,
 static int boost_write_wrapper(struct cgroup_subsys_state *css,
 			       struct cftype *cft, s64 boost)
 {
-	if (!strcmp(current->comm, "init"))
+	if (task_is_booster(current))
 		return 0;
 
 	return boost_write(css, cft, boost);
@@ -1019,7 +1020,7 @@ static int boost_write_wrapper(struct cgroup_subsys_state *css,
 static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
 				     struct cftype *cft, u64 prefer_idle)
 {
-	if (!strcmp(current->comm, "init"))
+	if (task_is_booster(current))
 		return 0;
 
 	return prefer_idle_write(css, cft, prefer_idle);
