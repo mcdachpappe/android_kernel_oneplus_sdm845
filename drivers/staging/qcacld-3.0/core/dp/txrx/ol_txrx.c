4334b0f7e74d9018c096d40430da4f42eb90df44
Kristof Petho <kristof.petho@gmail.com>
2020-01-31 10:21:32 +0100

    qcacld-3.0: fix unused variable warning
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/staging/qcacld-3.0/core/dp/txrx/ol_txrx.c
+++ drivers/staging/qcacld-3.0/core/dp/txrx/ol_txrx.c
@@ -3975,7 +3975,7 @@ static QDF_STATUS ol_txrx_clear_peer(struct cdp_pdev *ppdev, uint8_t sta_id)
  */
 void peer_unmap_timer_handler(void *data)
 {
-	ol_txrx_peer_handle peer = (ol_txrx_peer_handle)data;
+	//ol_txrx_peer_handle peer = (ol_txrx_peer_handle)data;
 
 	ol_txrx_err("all unmap events not received for peer %pK, ref_cnt %d",
 		    peer, qdf_atomic_read(&peer->ref_cnt));
