f667e83cdbc170e072774b562bb7506500bb889d
mcdachpappe <noreference@web.de>
2020-02-16 21:30:19 +0100

    cpu-boost: Hardcode dynamic_stune_boost
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -40,7 +40,7 @@ static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int dynamic_stune_boost;
+static int dynamic_stune_boost = 15;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
 static int boost_slot;
