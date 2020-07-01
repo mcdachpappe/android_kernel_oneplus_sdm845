1bdd47f5439e85fd5f73315820faf6b8db55ca95
mcdachpappe <noreference@web.de>
2020-07-01 15:08:08 +0200

    Revert "sched: rt: Use RCU lock in rt task cpu select path"
    As a wise man once said: 'I am pretty sure it would be better to
    revert caf's change and use Google's.' [renderbroken]
    
    This reverts commit 0cdc29e21c95dac9eaeacc47a265f6fb1e7e34d3.
    
    Signed-off-by: mcdachpappe <noreference@web.de>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1818,6 +1818,7 @@ static int find_lowest_rq(struct task_struct *task)
 				}
 			}
 		} while (sg = sg->next, sg != sd->groups);
+		rcu_read_unlock();
 
 		if (sg_target) {
 			cpumask_and(&search_cpu, lowest_mask,
@@ -1906,7 +1907,6 @@ retry:
 		}
 
 		if (best_cpu != -1 && placement_boost != SCHED_BOOST_ON_ALL) {
-			rcu_read_unlock();
 			return best_cpu;
 		} else if (!cpumask_empty(&backup_search_cpu)) {
 			cpumask_copy(&search_cpu, &backup_search_cpu);
@@ -1915,7 +1915,6 @@ retry:
 			placement_boost = SCHED_BOOST_NONE;
 			goto retry;
 		}
-		rcu_read_unlock();
 	}
 
 noea:
