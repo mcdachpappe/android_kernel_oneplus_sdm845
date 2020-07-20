79393ce5ae04a75e963ea8bf918e0155cafa35fc
mcdachpappe <noreference@web.de>
2020-07-20 22:59:31 +0200

    Revert "cpu-boost: increase Dynamic Schedtune Boost duration"
    This reverts commit 8dc6c7cdf74a5cf00b7e12b382de1e770a748ff0.
    
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -48,7 +48,7 @@ static int dynamic_stune_boost = 15;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
 static int boost_slot;
-static unsigned int dynamic_stune_boost_ms = 1500;
+static unsigned int dynamic_stune_boost_ms = 40;
 module_param(dynamic_stune_boost_ms, uint, 0644);
 static struct delayed_work dynamic_stune_boost_rem;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
