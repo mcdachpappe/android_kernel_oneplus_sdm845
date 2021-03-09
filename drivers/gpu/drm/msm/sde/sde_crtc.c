cda86c9f8968a2969c14e4079b22d0683c74a263
Art_Chen <Chenxy0201@qq.com>
2021-03-09 12:04:06 +0800

    drm: msm: Refactor fod dimming
    * Check oneplus_dimlayer_hbm_enable instead of
      fp_index to activate fod dimming mode.
    
    Co-authored-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: I8cd87ccab8e8dbbd0653ddbeadb5d8bafb72cd71
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -2985,6 +2985,7 @@ int bl_to_alpha_dc(int brightness)
 	return alpha;
 }
 
+bool oneplus_dimlayer_hbm_enable;
 int oneplus_get_panel_brightness_to_alpha(void)
 {
 	struct dsi_display *display = get_main_display();
@@ -3136,6 +3137,7 @@ ssize_t oneplus_display_notify_dim(struct device *dev,
 		return count;
 
 	oneplus_dim_status = dim_status;
+	oneplus_dimlayer_hbm_enable = oneplus_dim_status != 0;
 	if (oneplus_dim_status == 1 && HBM_flag) {
 		rc = dsi_panel_tx_cmd_set(display->panel, DSI_CMD_SET_HBM_ON_5);
 		if (rc) {
@@ -5355,8 +5357,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 	}
 	}
 	SDE_DEBUG("fp_index=%d,fppressed_index=%d,aod_index=%d\n", fp_index, fppressed_index, aod_index);
-	if (fp_index >= 0 || fppressed_index >= 0 ||
-		oneplus_force_screenfp || dim_backlight == 1) {
+	if (oneplus_dimlayer_hbm_enable || oneplus_force_screenfp || dim_backlight == 1) {
 		if (fp_index >= 0 && fppressed_index >= 0) {
 			if (pstates[fp_index].stage >=
 					pstates[fppressed_index].stage) {
@@ -5392,7 +5393,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 			zpos++;
 		}
 
-		if (fp_index >= 0)
+		if (oneplus_dimlayer_hbm_enable)
 			cstate->fingerprint_mode = true;
 		else
 			cstate->fingerprint_mode = false;
@@ -5410,6 +5411,7 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 			cstate->fingerprint_pressed = false;
 
 	} else {
+		cstate->fingerprint_dim_layer = NULL;
 		cstate->fingerprint_pressed = false;
 		cstate->fingerprint_mode = false;
 		for (i = 0; i < cnt; i++) {
@@ -5418,8 +5420,11 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 			}
 		}
 	}
-	if (fp_index < 0 && !dim_backlight)
-		cstate->fingerprint_dim_layer = NULL;
+	if (fp_mode == 1 && !oneplus_dimlayer_hbm_enable) {
+		cstate->fingerprint_mode = true;
+		cstate->fingerprint_pressed = true;
+		return 0;
+	}
 
 	return 0;
 }
