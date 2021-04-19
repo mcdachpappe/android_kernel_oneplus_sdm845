1f1ad4b8797e56fe71889432be944dbd92c1d285
mcdachpappe <noreference@web.de>
2021-04-20 00:23:17 +0200

    Revert "Revert "mm: make shrink_page_list with pages work from multiple zones""
    This reverts commit 27a37ccfd32d704db6da0c77466cef60192b7daf.
    
    Signed-off-by: mcdachpappe <noreference@web.de>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -979,6 +979,8 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			goto keep;
 
 		VM_BUG_ON_PAGE(PageActive(page), page);
+		if (pgdat)
+			VM_BUG_ON_PAGE(page_pgdat(page) != pgdat, page);
 
 		sc->nr_scanned++;
 
@@ -1066,7 +1068,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			/* Case 1 above */
 			if (current_is_kswapd() &&
 			    PageReclaim(page) &&
-			    test_bit(PGDAT_WRITEBACK, &pgdat->flags)) {
+			    (pgdat && test_bit(PGDAT_WRITEBACK, &pgdat->flags))) {
 				nr_immediate++;
 				goto activate_locked;
 
