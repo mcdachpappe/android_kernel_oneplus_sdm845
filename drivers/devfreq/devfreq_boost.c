e3f5056ba8575e660e9e76cb389cd5de01415c73
mcdachpappe <noreference@web.de>
2021-05-18 11:06:53 +0200

    devfreq_boost: Adapt driver code to our device
    * Use soc:qcom,cpubw instead of soc:qcom,cpu-llcc-ddr-bw
    * Use MSM_DRM_BLANK_UNBLANK_CUST as display state
    * Mark boost kthreads as performance critical only
      This amends commit 1618a7a8d723 ("devfreq_boost: Run boost kthreads on big CPU cluster")
    
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -10,7 +10,6 @@
 #include <linux/kthread.h>
 #include <linux/msm_drm_notify.h>
 #include <linux/slab.h>
-#include <uapi/linux/sched/types.h>
 
 enum {
 	SCREEN_OFF,
@@ -49,8 +48,8 @@ static void devfreq_max_unboost(struct work_struct *work);
 }
 
 static struct df_boost_drv df_boost_drv_g __read_mostly = {
-	BOOST_DEV_INIT(df_boost_drv_g, DEVFREQ_CPU_LLCC_DDR_BW,
-		       CONFIG_DEVFREQ_CPU_LLCC_DDR_BW_BOOST_FREQ)
+	BOOST_DEV_INIT(df_boost_drv_g, DEVFREQ_MSM_CPUBW,
+		       CONFIG_DEVFREQ_MSM_CPUBW_BOOST_FREQ)
 };
 
 static void __devfreq_boost_kick(struct boost_dev *b)
@@ -199,7 +198,7 @@ static int msm_drm_notifier_cb(struct notifier_block *nb,
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = &d->devices[i];
 
-		if (*blank == MSM_DRM_BLANK_UNBLANK) {
+		if (*blank == MSM_DRM_BLANK_UNBLANK_CUST) {
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
 				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
@@ -305,8 +304,7 @@ static int __init devfreq_boost_init(void)
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = &d->devices[i];
 
-		thread[i] = kthread_run_perf_critical(cpu_perf_mask,
-						      devfreq_boost_thread, b,
+		thread[i] = kthread_run_perf_critical(devfreq_boost_thread, b,
 						      "devfreq_boostd/%d", i);
 		if (IS_ERR(thread[i])) {
 			ret = PTR_ERR(thread[i]);
--- kernel/fork.c
+++ kernel/fork.c
@@ -2103,7 +2103,7 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current))
-		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
