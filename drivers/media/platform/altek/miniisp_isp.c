43340a4a303e5e436e093ee3650971b64db97a91
mcdachpappe <noreference@web.de>
2021-12-14 17:02:44 +0100

    media: platform: altek: Fix compiler warning
    Signed-off-by: mcdachpappe <noreference@web.de>

--- drivers/media/platform/altek/miniisp_isp.c
+++ drivers/media/platform/altek/miniisp_isp.c
@@ -2025,7 +2025,7 @@ static void __exit mini_isp_exit(void)
 		gpio_free(misp_drv_global_variable->irq_gpio);
 
 	/*if (misp_drv_global_variable)*/
-		kfree(misp_drv_global_variable);
+	kfree(misp_drv_global_variable);
 
 	/* unregister all driver */
 	spi_unregister_driver(&mini_isp_intf_spi);
